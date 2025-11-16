404ae944b3ed812f79b926b53840e32cbb5228bc
Luiz Thiago de Souza <luiz.thiago.souza@gmail.com>
2025-11-15 20:06:37 -0500

    Adds navigation and footer components
    Introduces a navigation bar for improved site navigation and a footer with links and copyright information.
    
    This enhances the user experience by providing clear navigation and essential site information.
    Removes a background color from the homepage.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,36 @@
+import Link from "next/link";
+import Image from "next/image";
+import Searchbar from "./Searchbar";
+import { Bell, Home, ShoppingCart } from "lucide-react";
+
 const Navbar = () => {
-  return <div className="">Navbar</div>;
+  return (
+    <nav className="w-full flex items-center justify-between border-b border-gray-200 pb-4">
+      {/* Left */}
+      <Link href="/" className="flex items-center">
+        <Image
+          src="/logo.png"
+          alt="TrendLama"
+          width={36}
+          height={36}
+          className="w-6 h-6 md:w-9 md:h-9"
+        />
+        <p className="hidden md:block text-ms font-medium tracking-wider">
+          TRENDLAMA.
+        </p>
+      </Link>
+      {/* Right */}
+      <div className="flex items-center gap-6">
+        <Searchbar />
+        <Link href="/">
+          <Home className="w-4 h-4 text-gray-600" />
+        </Link>
+        <Bell className="w-4 h-4 text-gray-600" />
+        <ShoppingCart className="w-4 h-4 text-gray-600" />
+        <Link href="/login">Sign in</Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
