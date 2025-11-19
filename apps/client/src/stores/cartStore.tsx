3d93dbd3c0c60bee3ba6fb5d9a2f9254b26da157
Luiz Thiago de Souza <luiz.thiago.souza@gmail.com>
2025-11-19 09:30:25 -0500

    feat: initialize e-commerce monorepo with Turbo
    - Added package.json for monorepo setup with Turbo, TypeScript, and Prettier.
    - Created ESLint configuration package with base, Next.js, and React-specific rules.
    - Established TypeScript configurations for base, Next.js, and React libraries.
    - Configured Turbo for task management and build processes.

---
+++ apps/client/src/stores/cartStore.tsx
@@ -0,0 +1,55 @@
+import { CartStoreActionsType, CartStoreStateType } from "@/types";
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+
+const useCartStore = create<CartStoreStateType & CartStoreActionsType>()(
+  persist(
+    (set) => ({
+      cart: [],
+      hasHydrated: false,
+      addToCart: (item) =>
+        set((state) => {
+          const existingItem = state.cart.findIndex(
+            (cartItem) =>
+              cartItem.id === item.id &&
+              cartItem.selectedSize === item.selectedSize &&
+              cartItem.selectedColor === item.selectedColor
+          );
+
+          if (existingItem !== -1) {
+            const updatedCart = [...state.cart];
+            updatedCart[existingItem]!.quantity += item.quantity || 1;
+            return { cart: updatedCart };
+          }
+
+          return { cart: [...state.cart, { ...item }] };
+        }),
+      removeFromCart: (item) =>
+        set((state) => ({
+          cart: state.cart.filter(
+            (cartItem) =>
+              !(
+                cartItem.id === item.id &&
+                cartItem.selectedSize === item.selectedSize &&
+                cartItem.selectedColor === item.selectedColor
+              )
+          ),
+        })),
+      clearCart: () =>
+        set(() => ({
+          cart: [],
+        })),
+    }),
+    {
+      name: "cart",
+      storage: createJSONStorage(() => localStorage),
+      onRehydrateStorage: () => (state) => {
+        if (state) {
+          state.hasHydrated = true;
+        }
+      },
+    }
+  )
+);
+
+export default useCartStore;
